4d41f8691997da389e33ac3f869ce00b4ce9deec
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-01-09 18:36:46 +0530

    worked on some styling

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,14 @@ function App() {
   };
 
   return (
-    <div
-      className="w-full h-screen flex flex-wrap justify-left items-center bg-auto bg-no-repeat"
-      style={{
-        backgroundImage: `url('https://images.pexels.com/photos/5912634/pexels-photo-5912634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-      }}
-    >
-      <div className="w-full">
+    <div className="h-screen lg:grid lg:grid-cols-2 lg:gap-x-6">
+      <div
+        className="bg-auto bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://images.pexels.com/photos/5912634/pexels-photo-5912634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+        }}
+      ></div>
+      <div className="relative mt-40 justify-right">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
